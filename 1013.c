66f3e67813c5d6219c445771296b4bdc022de673
rafaelavitoria14 <rafaela.vitoria@unifesp.br>
2026-06-24 20:30:29 -0300

    Update 1013.c

--- 1013.c
+++ 1013.c
@@ -11,14 +11,14 @@ Aprendizado : Uso da função abs e fórmula para encontrar o maior valor sem us
 #include <stdlib.h>
 
 int main() {
-    int a, b, c, maiorAB, maior;
-    
-    scanf("%d %d %d", &a, &b, &c);
-    
-    maiorAB = (a + b + abs (a - b)) / 2;
-    maior = (maiorAB + c + abs(maiorAB - c)) / 2;
-    
-    printf("%d eh o maior\n", maior);
-  
+    int a, b, c;
+
+    if (scanf("%d %d %d", &a, &b, &c) == 3) {
+        int maiorAB = (a + b + abs(a - b)) / 2;
+        int maiorTudo = (maiorAB + c + abs(maiorAB - c)) / 2;
+
+        printf("%d eh o maior\n", maiorTudo);
+    }
+
     return 0;
 }
