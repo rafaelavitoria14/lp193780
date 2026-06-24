21dfe48262dd4897717d227e3412bbed85289c5c
rafaelavitoria14 <rafaela.vitoria@unifesp.br>
2026-06-24 20:39:27 -0300

    Update 1017.c

--- 1017.c
+++ 1017.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Rafaela Vitória da Costa 
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1017
-Data        : 09/07/2026
+Data        : 09/06/2026
 Objetivo    : Calcular a quantidade de combustível gasta em uma viagem com base no tempo e na velocidade média.
 Aprendizado : Criar e utilizar uma função que retorna um valor float para realizar cálculos e exibir resultados formatados.
 -------------------------------------------------------------------------- */
