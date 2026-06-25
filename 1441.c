90f6fdae32c682d529e6f1550d899b8db03ebdf5
rafaelavitoria14 <rafaela.vitoria@unifesp.br>
2026-06-25 15:58:28 -0300

    Update 1441.c

--- 1441.c
+++ 1441.c
@@ -4,8 +4,8 @@ Nome        : Rafaela Vitória da Costa
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1441
 Data        : 16/06/2026
-Objetivo    : 
-Aprendizado : 
+Objetivo    : Construir e exibir uma matriz quadrada de ordem N cujos elementos formam camadas concêntricas com valores numéricos crescentes (começando em 1) da borda em direção ao centro.
+Aprendizado : Compreender como calcular o valor de cada posição de uma matriz com base na menor distância até uma de suas quatro bordas, utilizando uma lógica matemática direta com funções de mínimo.
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
