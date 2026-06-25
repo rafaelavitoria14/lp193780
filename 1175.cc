61255eeb58e59bd304fe757fdaf993bbf427fc6a
rafaelavitoria14 <rafaela.vitoria@unifesp.br>
2026-06-25 08:28:35 -0300

    Update 1175.cc

--- 1175.cc
+++ 1175.cc
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Rafaela Vitória da Costa 
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1175
+Data        : 23/06/2026
+Objetivo    : Ler um vetor de 20 elementos, inverter a ordem dos seus valores (trocando o primeiro com o último, o segundo com o penúltimo, etc.) e exibir o vetor modificado.
+Aprendizado : Dominar a manipulação de índices de arrays (std::vector ou vetores estáticos) em C++ e utilizar a técnica de dois ponteiros ou expressões matemáticas como 19 - i para inverter elementos de forma simétrica.
+-------------------------------------------------------------------------- */
+
 #include <iostream>
 
 int main() {
